cd9eb7a9e805f7119b48ef3f5d2e98efd791d6fc
Arfan Rahman Yudiantoro <arfanrahman@student.telkomuniversity.ac.id>
2023-05-16 18:18:29 +0700

    tes

--- app/src/main/java/com/celtic/furniplan/MainActivity.kt
+++ app/src/main/java/com/celtic/furniplan/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.get_started)
+        setContentView(R.layout.list_editprofile)
     }
 }
