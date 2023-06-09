2e583efda9228d50764c7223c9cce19f085c8801
Ihsan Hadiansyach <ihadiansyach@gmail.com>
2023-06-09 09:50:14 +0700

    add google-services.json and delete dialog load edit

--- app/src/main/java/com/celtic/furniplan/EditProfileActivity.kt
+++ app/src/main/java/com/celtic/furniplan/EditProfileActivity.kt
@@ -1,82 +1,17 @@
 package com.celtic.furniplan
 
-import android.app.Dialog
-import android.net.Uri
 import android.os.Bundle
-import android.view.Window
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.celtic.furniplan.databinding.ActivityEditProfileBinding
-import com.celtic.furniplan.model.User
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditProfileBinding
-    private lateinit var auth: FirebaseAuth
-    private lateinit var databaseReference: DatabaseReference
-    private lateinit var storageReference: StorageReference
-    private lateinit var imageUri: Uri
-    private lateinit var dialog: Dialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        auth = FirebaseAuth.getInstance()
-        val userId = auth.currentUser?.uid
-        databaseReference = FirebaseDatabase.getInstance().getReference("Users")
-        binding.btnSaveProfile.setOnClickListener {
-
-            showProgressBar()
-            val namaLengkap = binding.editTextNamalengkap.text.toString()
-            val userName = binding.editTextUsername.text.toString()
-            val email = binding.editTextEmail.text.toString()
-            val kataSandi = binding.editTextKataSandi.text.toString()
-
-            val user = User(namaLengkap, userName, email, kataSandi)
-            if (userId != null) {
-                databaseReference.child(userId).setValue(user).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        uploadProfilePic()
-                    } else {
-                        hideProgressBar()
-                        Toast.makeText(this, "Failed to update profile", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }
-    }
-
-    private fun uploadProfilePic() {
-        imageUri = Uri.parse("android.resource://$packageName/${R.drawable.upload_photo}")
-        storageReference =
-            FirebaseStorage.getInstance().getReference("Users/" + auth.currentUser?.uid)
-        storageReference.putFile(imageUri).addOnSuccessListener {
-
-            hideProgressBar()
-            Toast.makeText(this, "Profile success updated", Toast.LENGTH_SHORT).show()
-
-        }.addOnFailureListener {
-            hideProgressBar()
-            Toast.makeText(this, "Failed to update profile", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun showProgressBar() {
-        dialog = Dialog(this)
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setContentView(R.layout.dialog_load)
-        dialog.setCanceledOnTouchOutside(false)
-        dialog.show()
-    }
-
-    private fun hideProgressBar() {
-        dialog.dismiss()
     }
 }
